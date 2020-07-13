035d607f2c6eab2b7068c03983a9b8e6a0a4cda1
Ron Alonzo <alonzo.ronald@gmail.com>
2020-07-13 10:46:33 +0200

    setup nats message broker connection

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/dyzz/gobtclib v0.0.0-20160201094650-547ec01bd5e2
 	github.com/go-zeromq/zmq4 v0.10.0
+	github.com/nats-io/nats.go v1.10.0
 	github.com/pebbe/zmq4 v1.2.1
 	github.com/zeromq/goczmq v4.1.0+incompatible // indirect
 	google.golang.org/protobuf v1.25.0
--- main.go
+++ main.go
@@ -7,11 +7,14 @@ import (
 
 	"btc-node-proxy/listener"
 	"btc-node-proxy/server"
+	"btc-node-proxy/nats"
 )
 
 var (
 	btcNodeZmqAddr = os.Getenv("BTC_NODE_ZMQ_ADDR")
 	serverPort     = os.Getenv("PORT")
+	natsAddr 			 = os.Getenv("NATS_ADDR")
+	natsName 			 = os.Getenv("NATS_NAME")
 )
 
 func main() {
@@ -19,7 +22,12 @@ func main() {
 	log.Printf("env var %v\n", btcNodeZmqAddr)
 	wg := new(sync.WaitGroup)
 
-	wg.Add(2)
+	wg.Add(3)
+
+	go func() {
+		nats.Start(natsAddr, natsName)
+		wg.Done()
+	}
 
 	go func() {
 		server.Start(serverPort)
