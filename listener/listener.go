15e19316838b35354537352793b15ef005466ad2
RicardoAlonzo <alonzoricardo6@gmail.com>
2020-07-20 19:40:39 +0200

    pulled master

--- listener/listener.go
+++ listener/listener.go
@@ -5,12 +5,13 @@ import (
 	"log"
 
 	"github.com/pebbe/zmq4"
+	"github.com/piotrnar/gocoin/lib/btc"
 )
 
 func decode(src []byte) {
 	// log.Printf("decoding length: %v", len(src))
 	dst := make([]byte, hex.DecodedLen(len(src)))
-	log.Printf("dst: %v", dst)
+	log.Printf("DST: %v", dst)
 	log.Printf("HEX: %v", hex.EncodeToString(src))
 	// n, err := hex.Decode(dst, src)
 	// if err != nil {
@@ -26,39 +27,80 @@ func Start(btcNodeZmqAddr string) {
 
 	subscriber.Connect(btcNodeZmqAddr)
 
-	subscriber.SetSubscribe("hashblock")
+	// subscriber.SetSubscribe("hashblock")
 	// subscriber.SetSubscribe("hashtx")
-	// subscriber.SetSubscribe("rawblock")
+	subscriber.SetSubscribe("rawblock")
 	// subscriber.SetSubscribe("rawtx")
 
 	log.Println("Listening")
 	for {
-		e, addr, v, err := subscriber.RecvEvent(0)
+		frames, err := subscriber.RecvMessageBytes(0)
 		if err != nil {
 			log.Println(err)
 			continue
 		}
 
-		log.Printf("Event: %v", e)
-		addrs := []byte(addr)
-		log.Printf("Address: %v", addr)
-		decode(addrs)
-		log.Printf("Value: %v", v)
-		// bl, er := btc.NewBlock([]byte(addr))
-
-		// if er != nil {
-		// 	log.Println(err)
+		routingID := frames[0]
+		framez := frames[1:]
+		log.Printf("routingID: %v", routingID)
+		log.Printf("framez: %v", framez)
+		decode(framez[1])
+		decode(framez[0])
+		// if msg == "rawblock" {
 		// 	continue
 		// }
 
-		// log.Printf("Block hash: %v", bl.Hash)
-		// log.Printf("Block time: %v", bl.BlockTime)
-		// log.Printf("Block version: %v", bl.Version)
-		// log.Printf("Block parent: %v", btc.NewUint256(bl.Parent).String())
-		// log.Printf("Block merkle root: %v", hex.EncodeToString(bl.MerkleRoot))
-		// log.Printf("Block bits: %v", bl.Bits)
-		// log.Printf("Block size: %v", len(bl.Raw))
-		// decode([]byte(s))
-		// log.Println("rec", s)
+		// addrs := []byte(msg)
+		// log.Printf("BYTE: %v", addrs)
+		// decode(addrs)
+		bl, _ := btc.NewBlock(framez[0])
+
+		log.Printf("Block hash: %v", bl.Hash)
+		log.Printf("Block time: %v", bl.BlockTime)
+		log.Printf("Block version: %v", bl.Version)
 	}
 }
+
+// func Start(btcNodeZmqAddr string) {
+// 	subscriber, _ := zmq4.NewSocket(zmq4.SUB)
+// 	subscriber.SetLinger(0)
+
+// 	subscriber.Connect(btcNodeZmqAddr)
+
+// 	// subscriber.SetSubscribe("hashblock")
+// 	// subscriber.SetSubscribe("hashtx")
+// 	subscriber.SetSubscribe("rawblock")
+// 	// subscriber.SetSubscribe("rawtx")
+
+// 	log.Println("Listening")
+// 	for {
+// 		// e, addr, v, err := subscriber.RecvEvent(0)
+// 		msg, err := subscriber.Recv(0)
+// 		if err != nil {
+// 			log.Println(err)
+// 			continue
+// 		}
+
+// 		// log.Printf("Event: %v", e)
+// 		// addrs := []byte(addr)
+// 		// log.Printf("Address: %v", addr)
+// 		// decode(addrs)
+// 		// log.Printf("Value: %v", v)
+// 		bl, er := btc.NewBlock([]byte(addr))
+
+// 		if er != nil {
+// 			log.Println(err)
+// 			continue
+// 		}
+
+// 		log.Printf("Block hash: %v", bl.Hash)
+// 		log.Printf("Block time: %v", bl.BlockTime)
+// 		log.Printf("Block version: %v", bl.Version)
+// 		// log.Printf("Block parent: %v", btc.NewUint256(bl.Parent).String())
+// 		// log.Printf("Block merkle root: %v", hex.EncodeToString(bl.MerkleRoot))
+// 		log.Printf("Block bits: %v", bl.Bits)
+// 		log.Printf("Block size: %v", len(bl.Raw))
+// 		// decode([]byte(s))
+// 		// log.Println("rec", s)
+// 	}
+// }
--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -2,4 +2,6 @@
 
 PORT=4000 \
 BTC_NODE_ZMQ_ADDR=tcp://0.0.0.0:29000 \
+NATS_ADDR=nats://demo.nats.io:4222 \
+NATS_NAME=test-test-test \
 go run ./main.go
