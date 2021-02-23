08b381e64f2fb7cf7e3bdfc83beb832447e769a8
Ron Alonzo <alonzo.ronald@gmail.com>
2021-02-23 17:17:31 +0100

    test

--- healthServer.go
+++ healthServer.go
@@ -26,7 +26,7 @@ func healthCheck(res http.ResponseWriter, req *http.Request) {
 	currentTime := time.Now()
 
 	healthStatus := HealthStatus{
-		Status:    fmt.Sprintf("%v is Healthy!", serviceName),
+		Status:    fmt.Sprintf("%v is  Healthy!", serviceName),
 		TimeStamp: currentTime.Format("2006-01-02 15:04:05"),
 	}
 
